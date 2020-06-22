1507480f269db74ab5cc2d8666e7a52d0f32f0bf
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-06-22 04:48:25 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,6 +8,6 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else
-    return "NO, NOT SICNE 1938!"
+    return "HUH?! SPEAK UP, SONNY!"
 end
 end
