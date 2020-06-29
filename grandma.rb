b345cb95f6e2e3032efc921719469fca30d298ae
David Campbell <theatlanticpacific@gmail.com>
2020-06-29 18:04:19 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,15 @@
+def speak_to_grandma(string)
+if string != string.upcase
+return "HUH?! SPEAK UP, SONNY!"
+if puts "I LOVE YOU GRANDMA!"
+return "I LOVE YOU TOO PUMPKIN"   
+elsif string.upcase
+return "NO, NOT SINCE 1938!"
+
+
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
